d23c9060265bb466954b7e9c50a97d414a501011
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2026-01-31 02:37:20 +0100

    fix: local auth

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,18 +1,3 @@
-"""myproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
@@ -21,18 +6,11 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from myapp import views as myapp_views
 
-urlpatterns_i18n = i18n_patterns(
-    path('admin/', admin.site.urls),
-    path("", include("myapp.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path("i18n/", include("django.conf.urls.i18n")),
-)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", myapp_views.smart_login, name="login"),
     path("", include("myapp.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/login/", myapp_views.smart_login, name="login"),
 ]
 
 # Conditionally include OIDC URLs if OIDC_ENABLED is False
